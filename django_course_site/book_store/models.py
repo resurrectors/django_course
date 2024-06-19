7608a3369000f0566942d3cec754236aa9a3b8fe
Shubham Sanjay Patil <shubham-sanjay.patil@hpe.com>
2024-06-19 15:53:33 +0530

    updating models and migrations

--- django_course_site/book_store/models.py
+++ django_course_site/book_store/models.py
@@ -6,3 +6,8 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField()
+    author = models.CharField(null=True, max_length=100)
+    is_bestselling = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return f"{self.title}-{self.rating}"
