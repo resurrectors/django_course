72ccf8be1e1d73ce829dbc2438faff18f1bc1321
Shubham Sanjay Patil <shubham-sanjay.patil@hpe.com>
2024-06-19 16:48:24 +0530

    Preparing Templates

--- django_course_site/book_store/views.py
+++ django_course_site/book_store/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from . import views
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, "book_store/books.html")
--- django_course_site/django_course_site/urls.py
+++ django_course_site/django_course_site/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('challenges/', include("challenges.urls"))
+    path('challenges/', include("challenges.urls")),
+    path('', include("book_store.urls"))
 ]
