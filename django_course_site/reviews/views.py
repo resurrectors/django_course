4e1a95d7d78a43c6dcf6382c34b261018b425a80
Shubham Sanjay Patil <shubham-sanjay.patil@hpe.com>
2024-06-21 13:14:27 +0530

    Simple form implementation with validation

--- django_course_site/reviews/views.py
+++ django_course_site/reviews/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
-
+from django.shortcuts import HttpResponseRedirect
+from .forms import ReviewForm
 # Create your views here.
 
 
 def index(request):
-    return render(request, "reviews/review.html")
+
+    if request.method == "POST":
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            return HttpResponseRedirect("/thank-you")
+
+    form = ReviewForm()
+    return render(request, "reviews/review.html", {
+        "form": form
+    })
+
+
+def thankyou(request):
+    return render(request, "reviews/thankyou.html")
