739576fcb7aa517b01e610ad64ab2e2295483ba7
Shubham Sanjay Patil <shubham-sanjay.patil@hpe.com>
2024-06-21 08:43:40 +0530

    Many-to-one relationship of models

--- django_course_site/book_store/models.py
+++ django_course_site/book_store/models.py
@@ -3,10 +3,18 @@ from django.db import models
 # Create your models here.
 
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return f"{self.first_name} - {self.last_name}"
+
+
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField()
-    author = models.CharField(null=True, max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(null=True)
 
--- django_course_site/book_store/views.py
+++ django_course_site/book_store/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from . import views
-
+from .models import Book, Author
 # Create your views here.
 
 
 def index(request):
-    return render(request, "book_store/books.html")
+    authors = Author.objects.all()
+    books = Book.objects.all()
+    num_books = books.count()
+    return render(request, "book_store/books.html", {
+        "total_no_of_books": num_books,
+        "books": books,
+        "authors": authors
+    })
