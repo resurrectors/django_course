0f3722071c766fa4a4fb2bf46f16de54ba225e6d
Shubham Sanjay Patil <shubham-sanjay.patil@hpe.com>
2024-06-21 18:11:35 +0530

    Customizing the rendered HTML form

--- django_course_site/reviews/forms.py
+++ django_course_site/reviews/forms.py
@@ -2,4 +2,7 @@ from django import forms
 
 
 class ReviewForm(forms.Form):
-    user_name = forms.CharField()
+    user_name = forms.CharField(label="Your Name", max_length=100, error_messages={
+        "required": "Yo this is required",
+        "max_length": "Yo make it short bro"
+    })
